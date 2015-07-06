fb3ad23aed9371d2b02d9c3de766d4a144f6670f
yoggy <yoggy0@gmail.com>
2015-07-06 14:54:50 +0900

    added src_host,dst_host

--- mqtt_conn_log.rb
+++ mqtt_conn_log.rb
@@ -4,24 +4,36 @@ require 'rubygems'
 require 'mqtt'
 require 'pit'
 require 'json'
+require 'resolv'
 require 'pp'
 
 $stdout.sync = true
 
 $config = Pit.get("mqtt_conn_log", :require => {
-	"mqtt_host"  => "mqtt.example.com",
-	"mqtt_port"  => 1883,
-	"mqtt_user"  => "mqtt_user",
-	"mqtt_pass"  => "mqtt_pass",
-	"mqtt_topic" => "mqtt_topic",
+    "mqtt_host"  => "mqtt.example.com",
+    "mqtt_port"  => 1883,
+    "mqtt_user"  => "mqtt_user",
+    "mqtt_pass"  => "mqtt_pass",
+    "mqtt_topic" => "mqtt_topic",
 })
 
 def split_ip_port(str)
-  ridx = str.rindex(".")
-  ip   = str[0, ridx]
-  port = str[ridx+1, str.size - ridx]
+    ridx = str.rindex(".")
+    ip   = str[0, ridx]
+    port = str[ridx+1, str.size - ridx]
+    
+    [ip, port]
+end
 
-  [ip, port]
+def gethostbyname(ip_str)
+    host = ip_str
+    
+    begin
+        host = Resolv.getname(ip_str)
+    rescue Exception => e
+    end
+    
+    host
 end
 
 MQTT::Client.connect(
@@ -58,12 +70,17 @@ MQTT::Client.connect(
     
         (src_ip, src_port) = split_ip_port(src_str)
         (dst_ip, dst_port) = split_ip_port(dst_str)
+
+        src_host = gethostbyname(src_ip)
+        dst_host = gethostbyname(dst_ip)
     
         h = {}
         h["timestamp"] = v[0]
         h["src_ip"]    = src_ip
+        h["src_host"]  = src_host
         h["src_port"]  = src_port
         h["dst_ip"]    = dst_ip
+        h["dst_host"]  = dst_host
         h["dst_port"]  = dst_port
     
         publish_str = JSON.generate(h)
